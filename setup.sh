ecdcaa06592398de82568591afeca9def0f98c95
khurram shakoor <khurramer2018@gmail.com>
2021-11-27 18:13:13 +0530

    10

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 mkdir -p ~/.streamlit/
 
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
 echo "[theme]
-primaryColor = "#84a3a7"
-backgroundColor = "#EFEDE8"
-secondaryBackgroundColor = "#fafafa"
-textColor= "#424242"
-font = "sans serif"
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
 [server]
 headless = true
 port = $PORT
